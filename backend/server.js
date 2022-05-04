bf556f8a8885931ccdd75308455d9042fd414d85
clibretto <98610027+clibretto@users.noreply.github.com>
2022-05-04 13:52:40 -0400

    Redirect and authenticate

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
+import auth from './auth.js';
 import { game36Database } from './database.js';
-
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 class game36Server {
   constructor(dburl) {
     this.dburl = dburl;
@@ -14,22 +16,51 @@ class game36Server {
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use(logger('dev'));
     this.app.use('/client', express.static('client'));
+    
+
+    auth.configure(this.app);
+
     // Note: when using arrow functions, the "this" binding is lost.
     const self = this;
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(dirname(__filename));
+    const sessionConfig = {
+        secret: process.env.SECRET || 'SECRET',
+        resave: false,
+        saveUninitialized: false,
+    };
+    this.app.use(expressSession(sessionConfig));
 
+    function checkLoggedIn(request, response, next) {
+       try {
+           if (request.isAuthenticated()) {
+          // If we are authenticated, run the next route.
+          next();
+        } else {
+          // Otherwise, redirect to the login page.
+          response.redirect('/client/login/login.html');
+        }
+      } catch (err) {
+        response.status(500).send(err);
+    }};
     this.app.post('/register', async function (request, response) {
         try {
             const { username, password } = request.query;
             const newUser = await self.db.createUser(username, password);
             if(newUser === false){
-                response.status(200).send(false);
+                response.status(200).send({'status': 'success'});
+                response.redirect('/client/login/login.html')
             }
             else{
-                response.status(200).send(true);
+                response.status(200).send({'status': 'success'});
+                response.redirect('/client/signup/signup.html');
             }
         } catch (err) {
             response.status(500).send(err);
         }
+    });
+    this.app.get('/register', function (request, response) {
+        response.sendFile('/client/signup/signup.html', { root: __dirname })
     });
 
     this.app.post('/addPost', async function (request, response) {
@@ -71,24 +102,23 @@ class game36Server {
             response.status(500).send(err);
         }
     });
-
-    this.app.get('/login', async function (request, response) {
-        const { username, password } = request.query;
-        try {
-            const res = await self.db.confirmUser(username, password);
-            console.log(res);  
-            if(res){
-                response.status(200).send(true);
-            }
-            else{
-                response.status(200).send(false);
-            }
-        } catch (err) {
-            response.status(500).send(err);
-        }
+    //use auth
+    this.app.post(
+  '/login',
+  auth.authenticate('local', {
+    // use username/password authentication
+    successRedirect: '/client/gamecon.html', // when we login, go to /private
+    failureRedirect: '/client/login/login.html', // otherwise, back to login
+  })
+);
+    this.app.get('/login', function (response){
+        response.sendFile('/client/login/login.html', { root: __dirname })
     });
-
-    this.app.get('/getPosts', async function (request, response) {
+    this.app.get('/logout', function (request, response) {
+        request.logout(); // Logs us out!
+        response.redirect('/login'); // back to login 
+    });
+    this.app.get('/getPosts', async function (response) {
         try{
             let posts = await self.db.readPosts();
             response.status(200).send(posts);
@@ -112,11 +142,13 @@ class game36Server {
         }
     });
 
-    this.app.get('/', (request, response) => {
-        response.redirect('/client/gamecon.html');
-    })
-    //
-  }
+    
+    this.app.get('/',checkLoggedIn,function (request, response)  {
+      // Go to the main page.
+      response.redirect('/client/gamecon.html');
+    }
+  );
+}
 
   async initDb() {
     this.db = new game36Database(this.dburl);
